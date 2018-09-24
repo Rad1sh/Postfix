41ff9aada608793910e9a76bde2497d1ffa77f0d
Rad1sh <43543850+Rad1sh@users.noreply.github.com>
2018-09-25 00:08:21 +0700

    commit

---
+++ HW02/HW02/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace HW02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(3, 3);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(794, 219);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(382, 425);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(2, -2);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(797, 450);
+            this.splitContainer1.SplitterDistance = 225;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.label1.Location = new System.Drawing.Point(41, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "OP.jpg");
+            this.imageList1.Images.SetKeyName(1, "NU.jpg");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        public System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ HW02/HW02/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+namespace HW02
+{
+
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        public string input = Console.ReadLine();
+
+
+        public static int ComputeInfix(string infix)
+        {
+            var operatorstack = new Stack<char>();
+            var operandstack = new Stack<int>();
+
+            var precedence = new Dictionary<char, int> { { '(', 0 }, { '*', 1 }, { '/', 1 }, { '+', 2 }, { '-', 2 }, { ')', 3 } };
+
+            foreach (var ch in infix)
+            {
+                switch (ch)
+                {
+                    case var digit when Char.IsDigit(digit):
+                        operandstack.Push(Convert.ToInt32(digit.ToString()));
+                        break;
+                    case var op when precedence.ContainsKey(op):
+                        var keepLooping = true;
+                        while (keepLooping && operatorstack.Count > 0 && precedence[ch] > precedence[operatorstack.Peek()])
+                        {
+                            switch (operatorstack.Peek())
+                            {
+                                case '+':
+                                    operandstack.Push(operandstack.Pop() + operandstack.Pop());
+                                    break;
+                                case '-':
+                                    operandstack.Push(-operandstack.Pop() + operandstack.Pop());
+                                    break;
+                                case '*':
+                                    operandstack.Push(operandstack.Pop() * operandstack.Pop());
+                                    break;
+                                case '/':
+                                    var divisor = operandstack.Pop();
+                                    operandstack.Push(operandstack.Pop() / divisor);
+                                    break;
+                                case '(':
+                                    keepLooping = false;
+                                    break;
+                            }
+                            if (keepLooping)
+                                operatorstack.Pop();
+                        }
+                        if (ch == ')')
+                            operatorstack.Pop();
+                        else
+                            operatorstack.Push(ch);
+                        break;
+                    default:
+                        throw new ArgumentException();
+                }
+            }
+
+            if (operatorstack.Count > 0 || operandstack.Count > 1)
+                throw new ArgumentException();
+
+            return operandstack.Pop();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string result = "";
+           
+            result = rec(e.Node, result);
+            label1.Text = result + " = " + ComputeInfix(result);
+        }
+
+        public string rec(TreeNode ParentNode, string equation)
+        {
+   
+            string eq = equation;
+            TreeNode tempNode = ParentNode;
+
+
+            int NodeCounter = 0;
+            foreach (TreeNode currentNode in tempNode.Nodes)
+                NodeCounter++;
+
+            if(NodeCounter != 0)
+            eq = "(" + eq;
+            if (NodeCounter >= 1)
+            {
+                eq = rec(tempNode.Nodes[0],eq);
+              
+            }
+     
+            eq = eq + tempNode.Text;
+           
+
+            if (NodeCounter >= 2)
+            {
+                eq = rec(tempNode.Nodes[1], eq);
+            
+            }
+            if (NodeCounter != 0)
+                eq = eq + ")";
+
+           
+            return eq;
+        }
+
+        public int calculate(int a, int b ,string op)
+        {
+            if (op == "+")
+                return a + b;
+            else if (op == "-")
+                return a - b;
+            else if (op == "*")
+                return a * b;
+            else if (op == "/")
+                return a / b;
+            return 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+
+            Node rootNode = null;
+            Tree tree = new Tree();
+
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                rootNode = tree.insert(rootNode, input[i]);
+
+            }
+
+
+            TreeNode tNode = new TreeNode();
+            // TreeView t2 = new TreeView();
+
+
+            tNode.Nodes.Add("CALCULATOR");
+            //tNode.Nodes[0].Nodes.Add("root_directory 2");
+
+
+            TreeNode resultNode = tree.recursiveNode(rootNode, tNode.Nodes[0]);
+
+            //resultNode.Nodes.Remove(resultNode.FirstNode);
+
+
+            treeView1.Nodes.Add(resultNode.Nodes[0]);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+    class Node
+    {
+        public char value = ' ';
+        public Node left = null;
+        public Node right = null;
+    }
+
+    class Tree
+    {
+        bool isAdded = false;
+        public Node insert(Node root, char v)
+        {
+            isAdded = false;
+            if (root == null)
+            {
+                Console.WriteLine("Initial Insert " + v + "\n----------\n");
+                root = new Node();
+                root.value = v;
+                isAdded = true;
+            }
+            else
+            {
+                if (isOperator(root.value) == true && isAdded == false)
+                {
+                    Console.WriteLine("go Left " + root.value);
+                    root.left = insert(root.left, v);
+                }
+                if (isOperator(root.value) == true && isAdded == false)
+                {
+                    Console.WriteLine("go Right " + root.value);
+                    root.right = insert(root.right, v);
+                }
+                if (isAdded == false)
+                {
+                    Console.WriteLine("back to parent");
+                }
+            }
+            return root;
+        }
+
+
+        public TreeNode recursiveNode(Node parent, TreeNode tree)
+        {
+
+            Node temp = parent;
+            TreeNode tempTree = tree;
+            tempTree.Nodes.Add(temp.value.ToString());
+            //tempTree.Text = temp.value+"";
+
+            if (temp.left != null)
+            {
+               
+                TreeNode tm = recursiveNode(temp.left, tempTree.LastNode);
+                tempTree.Nodes.Remove(tempTree.LastNode);
+                tempTree.Nodes.Add(tm);
+                
+
+
+            }
+            if (temp.right != null)
+            {
+               
+                TreeNode tm = recursiveNode(temp.right, tempTree.LastNode);
+                tempTree.Nodes.Remove(tempTree.LastNode);
+                tempTree.Nodes.Add(tm);
+
+
+
+            }
+
+           
+
+
+            return tempTree;
+
+            
+        }
+
+        public static bool isNumber(char n)
+        {
+            if (n >= '0' && n <= '9')
+                return true;
+            else
+                return false;
+        }
+        public static bool isOperator(char n)
+        {
+            if (n == '+' || n == '-' || n == '*' || n == '/')
+                return true;
+            else
+                return false;
+        }
+    }
+
+
+}
